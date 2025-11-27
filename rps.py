1f765c54b24f6873e15c8f499f36d32a445c0526
Evilpasture <mandatorythebest@gmail.com>
2025-11-27 18:43:31 +0700

    Load license text from LICENSE file

--- rps.py
+++ rps.py
@@ -27,31 +27,12 @@ class GUI:
                                                                                        Rock Paper Scissors made by a university student from Data Science & Artificial Intelligence major.
                                                                                        Created for a university project.
                                                                                        """))
+
+        with open("LICENSE", "r") as f:
+            _license = f.read()
+        
         self.about_menu.add_separator()
-        self.about_menu.add_command(label="License", command=lambda: messagebox.showinfo("License",
-                                                                                         """
-                                                                                         Copyright (c) 2025
-                                                                                         
-                                                                                         Permission is hereby granted, free of charge, to any person obtaining a copy
-                                                                                         of this software and associated documentation files (the "Software"), to deal
-                                                                                         in the Software without restriction, including without limitation the rights
-                                                                                         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                                                                                         copies of the Software, and to permit persons to whom the Software is
-                                                                                         furnished to do so, subject to the following conditions:
-                                                                                         
-                                                                                         The above copyright notice and this permission notice shall be included in
-                                                                                         all copies or substantial portions of the Software.
-                                                                                         
-                                                                                         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                                                                                         IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                                                                                         FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                                                                                         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                                                                                         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                                                                                         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-                                                                                         THE SOFTWARE.
-                                                                                         
-                                                                                         SPDX-License-Identifier: MIT
-                                                                                         """))
+        self.about_menu.add_command(label="License", command=lambda: messagebox.showinfo("License", _license))
 
         self.settings_menu = tk.Menu(self.menubar, tearoff=0)
         self.settings_menu.add_command(label="Settings", command=self.open_settings)
